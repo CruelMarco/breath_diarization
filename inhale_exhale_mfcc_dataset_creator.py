0c77be075e818e1b770daccc98c3026815471fc4
Shaique <solankishaique@gmail.com>
2022-12-09 20:05:48 +0530

    inhale and exhale mfcc dataset

---
+++ inhale_exhale_mfcc_dataset_creator.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+
+import IPython
+import matplotlib.pyplot as plt
+import numpy as np
+import soundfile as sf
+from tqdm import tqdm
+import os
+from scipy.io.wavfile import write
+import scipy.signal
+from spectrum import aryule
+from pylab import plot, axis, xlabel, ylabel, grid, log10
+import scipy.signal
+from nara_wpe.wpe import wpe
+from nara_wpe.wpe import get_power
+from nara_wpe.utils import stft, istft, get_stft_center_frequencies
+from nara_wpe import project_root
+import os
+import librosa
+import pandas as pd
+from pandas import DataFrame as df
+import glob, os
+import shutil
+import json
+import sklearn
+import shutil
+import math
+from operator import itemgetter
+
+#os.chdir('C:/Users/Spirelab/Desktop/Breath_gender/Shivani_data')
+
+dir = 'C:/Users/Spirelab/Desktop/Breath_gender/Shivani_data/test_recs'
+
+os.chdir(dir)
+
+mfcc_store_dir = 'C:/Users/Spirelab/Desktop/Breath_Diarization/mfcc_d_dd_ii_xx'
+
+files = os.listdir(dir)
+
+wav_files = [f for f in files if f.endswith(".wav")]
+
+txt_files = [f for f in files if f.endswith(".txt")]
+
+male_count = 0
+
+female_count = 0
+    
+############# MFCC Calculation ###############
+
+for j in tqdm(wav_files) :
+    
+    audio_path = os.path.join(dir, j)
+    
+    audio_file, fs = librosa.load(audio_path, sr = 16000, mono = True)
+    
+    annot_path = audio_path[0 : -3] + 'txt'
+    
+    annot_file = pd.read_csv(annot_path , sep = "\t", names = ['start', 'end', 'phon'] , header = None)
+    
+    gender = j.split("_")[8]
+    
+    if gender == 'M' :
+        
+        male_count+=1
+    else :
+        
+        female_count+=1
+    
+    name = j.split("_")[5]
+    
+    phon_col = annot_file['phon']
+    
+    st_col = annot_file['start']
+    
+    end_col = annot_file['end']
+    
+    ###Inhale DF###
+    
+    ii_idx = [i for i in range(len(phon_col)) if "Inhale" in phon_col[i]]
+    
+    ii_st_idx = st_col[ii_idx]
+    
+    ii_end_idx = end_col[ii_idx]
+    
+    ii_st_sam = list(np.ceil(ii_st_idx * fs))
+    
+    ii_end_sam = list(np.ceil(ii_end_idx*fs))
+    
+    ii_st_sam = [int(i) for i in ii_st_idx]
+    
+    ###Exhale DF###
+    xx_idx = [j for j in range(len(phon_col)) if "Exhale" in phon_col[j]]
+    
+    xx_st_idx = st_col[xx_idx]
+    
+    xx_end_idx = end_col[xx_idx]
+    
+    xx_st_sam = list(np.ceil(ii_st_idx * fs))
+    
+    xx_end_sam = list(np.ceil(xx_end_idx*fs))
+    
+    xx_st_sam = [int(j) for j in xx_st_idx]    
+    
+    
+    
+    
+    ii_chunk = []
+    
+    ii_chunk_mfcc_df = []
+    
+    sub_ii_mfcc_df = []
+    
+    #print(j)
+    
+    for k in range(len(ii_st_sam)) :
+        
+        ii_chunk = audio_file[int(ii_st_sam[k]) : int(ii_end_sam[k])]
+        
+        mfcc_ii = librosa.feature.mfcc(ii_chunk , sr = fs , n_mfcc = 13 , n_fft = 320 , win_length = 320 , hop_length = 160)
+                
+        ii_chunk_mfcc_df = np.array(np.transpose(librosa.feature.mfcc(ii_chunk , sr = fs , n_mfcc = 13 , n_fft = 320 , win_length = 320 , hop_length = 160)))
+        
+        delta = np.array(np.transpose(librosa.feature.delta(mfcc_ii)))
+        
+        delta_ii_chunk_mfcc_df = pd.DataFrame(delta , columns = ['F0_d' , 'F1_d' , 'F2_d' , 'F3_d' , 'F4_d' , 'F5_d' , 'F6_d' , 'F7_d' , 'F8_d' , 'F9_d' , 'F10_d' , 'F11_d' , 'F12_d'])
+
+        delta2 = np.array(np.transpose(librosa.feature.delta(mfcc_ii, order=2)))
+        
+        delta2_ii_chunk_mfcc_df = pd.DataFrame(delta2 , columns = ['F0_dd' , 'F1_dd' , 'F2_dd' , 'F3_dd' , 'F4_dd' , 'F5_dd' , 'F6_dd' , 'F7_dd' , 'F8_dd' , 'F9_dd' , 'F10_dd' , 'F11_dd' , 'F12_dd'])
+        
+        ii_chunk_mfcc_df = pd.DataFrame(ii_chunk_mfcc_df , columns = ['F0' , 'F1' , 'F2' , 'F3' , 'F4' , 'F5' , 'F6' , 'F7' , 'F8' , 'F9' , 'F10' , 'F11' , 'F12'])
+        
+        index_df = pd.DataFrame([ii_idx[k] ]*ii_chunk_mfcc_df.shape[0], columns = ['phon_idx'])
+        
+        mfcc_ii_delta_delta2 = pd.concat([index_df , ii_chunk_mfcc_df, delta_ii_chunk_mfcc_df, delta2_ii_chunk_mfcc_df],axis=1, join='inner')
+        
+        #print(mfcc_ii_delta_delta2)
+        
+        sub_ii_mfcc_df.append(mfcc_ii_delta_delta2)
+        
+    sub_ii_mfcc_df = pd.concat(sub_ii_mfcc_df, ignore_index=True)
+     
+    sub_ii_mfcc_df.insert(0,'Name' , name , True)
+    
+    phon_ii = "ii"
+    
+    sub_ii_mfcc_df['phon'] = phon_ii
+    
+    xx_chunk = []
+    
+    xx_chunk_mfcc_df = []
+    
+    sub_xx_mfcc_df = []
+    
+    
+    
+    for q in range(len(xx_st_sam)) :
+        
+        xx_chunk = audio_file[int(xx_st_sam[q]) : int(xx_end_sam[q])]
+        
+        mfcc_xx = librosa.feature.mfcc(xx_chunk , sr = fs , n_mfcc = 13 , n_fft = 320 , win_length = 320 , hop_length = 160)
+                
+        xx_chunk_mfcc_df = np.array(np.transpose(librosa.feature.mfcc(xx_chunk , sr = fs , n_mfcc = 13 , n_fft = 320 , win_length = 320 , hop_length = 160)))
+        
+        delta = np.array(np.transpose(librosa.feature.delta(mfcc_xx)))
+        
+        delta_xx_chunk_mfcc_df = pd.DataFrame(delta , columns = ['F0_d' , 'F1_d' , 'F2_d' , 'F3_d' , 'F4_d' , 'F5_d' , 'F6_d' , 'F7_d' , 'F8_d' , 'F9_d' , 'F10_d' , 'F11_d' , 'F12_d'])
+
+        delta2 = np.array(np.transpose(librosa.feature.delta(mfcc_xx, order=2)))
+        
+        delta2_xx_chunk_mfcc_df = pd.DataFrame(delta2 , columns = ['F0_dd' , 'F1_dd' , 'F2_dd' , 'F3_dd' , 'F4_dd' , 'F5_dd' , 'F6_dd' , 'F7_dd' , 'F8_dd' , 'F9_dd' , 'F10_dd' , 'F11_dd' , 'F12_dd'])
+        
+        xx_chunk_mfcc_df = pd.DataFrame(xx_chunk_mfcc_df , columns = ['F0' , 'F1' , 'F2' , 'F3' , 'F4' , 'F5' , 'F6' , 'F7' , 'F8' , 'F9' , 'F10' , 'F11' , 'F12'])
+        
+        index_xx_df = pd.DataFrame([xx_idx[q] ]*xx_chunk_mfcc_df.shape[0], columns = ['phon_idx'])
+        
+        mfcc_xx_delta_delta2 = pd.concat([index_xx_df , xx_chunk_mfcc_df, delta_xx_chunk_mfcc_df, delta2_xx_chunk_mfcc_df],axis=1, join='inner')
+        
+        #print(mfcc_ii_delta_delta2)
+        
+        sub_xx_mfcc_df.append(mfcc_xx_delta_delta2)
+    
+    sub_xx_mfcc_df = pd.concat(sub_xx_mfcc_df, ignore_index=True)
+     
+    sub_xx_mfcc_df.insert(0,'Name' , name , True)
+    
+    phon_xx = "xx"
+    
+    sub_xx_mfcc_df['phon'] = phon_xx
+    
+    sub_ii_xx_mfcc_df = pd.concat([sub_ii_mfcc_df , sub_xx_mfcc_df], axis = 0)
+    
+    sub_ii_xx_mfcc_df = sub_ii_xx_mfcc_df.sort_values(by = ['phon_idx'])
+    
+    
+    mfcc_name = j[0 : -4] + '_iixx_d_dd''.csv'
+    
+    mfcc_df_dir = os.path.join(mfcc_store_dir , mfcc_name)
+    
+    sub_ii_xx_mfcc_df.to_csv(mfcc_df_dir)
+    
+print("No. of Male controls = " , male_count)
+
+print("No. of Female controls = " , female_count)
+        
+        
+    
+    
+    
+    
+
+
